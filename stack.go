5031691f6c1cce6327a88101c68de81c56169bf4
Till Burdorf <burdorftill@gmail.com>
2025-02-20 12:11:48 +0100

    removed unused folder

---
+++ stack.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Stack struct {
+	items []string
+}
+
+// Pushes a new element on the Stacl
+func (stack *Stack) Push(item string) {
+	stack.items = append(stack.items, item)
+}
+
+// Pops the first element from the Stack
+func (s *Stack) Pop() (string, error) {
+	if len(s.items) == 0 {
+		return "", fmt.Errorf("Stack ist leer")
+	}
+	lastIndex := len(s.items) - 1
+	item := s.items[lastIndex]
+	s.items = s.items[:lastIndex]
+	return item, nil
+}
+
+func getTopTwoValuesAsFloat(stack Stack) (float64, float64) {
+	stack.Pop()
+	first, _ := stack.Pop()
+	second, _ := stack.Pop()
+
+	firstFloat, _ := strconv.ParseFloat(first, 64)
+	secondFloat, _ := strconv.ParseFloat(second, 64)
+
+	return firstFloat, secondFloat
+}
+
+func getTopValueAsFloat(stack Stack) float64{
+	stack.Pop()
+	value, _ := stack.Pop()
+	valueFloat, _ := strconv.ParseFloat(value, 64)
+	return valueFloat
+}
